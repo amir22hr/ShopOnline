ae2cc8025dc3a6103482d389ddab8556a72cecb6
amir22hr <info.amir22hr@gmail.com>
2021-12-24 15:22:19 +0330

    update checkout

--- controllers/Products/checkoutController.js
+++ controllers/Products/checkoutController.js
@@ -4,37 +4,19 @@ const nodemailer = require('nodemailer');
 const ejs = require('ejs');
 const path = require('path')
 const showCart = require('../../helpers/products/showCart');
+const mailSender = require('../../helpers/Auth/mailSender');
 
 const checkoutController = async (req, res) => {
 
     if (req.query.id) {
 
-        //send Mail Validation
-        var transporter = await nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.GMAIL,
-                pass: process.env.GMAIL_PASS
-            },
-        });
         //render HTML
         const html = await ejs.renderFile(path.join(__dirname, '../../views/mail/checkout.ejs'), {
             title: "Thanks for buying from us",
             list: await showCart(req.user)
         })
-        //form mail
-        const mailOptions = {
-            from: process.env.GMAIL, // sender address
-            to: req.user.email, // list of receivers
-            subject: 'checkout', // Subject line
-            html: html// plain text body
-        };
-        await transporter.sendMail(mailOptions, function (err, info) {
-            if (err)
-                console.log(err)
-            else
-                console.log(info);
-        });
+
+        mailSender(req.user.email, 'checkout', html)
 
         await Customers.findOneAndUpdate(
             //req.query.id => user
